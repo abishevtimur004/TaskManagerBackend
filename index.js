3c595cfa20da5135e3886bc5ece731697acf159b
Timur Abishev <abisevtimur221@hmail.com>
2025-04-17 21:27:14 +0500

    обновил CORS

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const startServer = async () => {
 
     app.use(
       cors({
-        // origin: "http://localhost:5173",
-        // credentials: true,
+        origin: "http://localhost:5173",
+        credentials: true,
       })
     );
     app.use(express.json());
